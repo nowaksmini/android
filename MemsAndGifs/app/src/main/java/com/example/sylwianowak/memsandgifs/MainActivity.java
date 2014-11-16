ebdac13061c7cdf20b2adb2e5a97364b323db0f5
Sylwia Nowak <Sylwia.Nowak@cybercom.com>
2014-11-16 23:47:42 +0100

    log out after click settings

--- MemsAndGifs/app/src/main/java/com/example/sylwianowak/memsandgifs/MainActivity.java
+++ MemsAndGifs/app/src/main/java/com/example/sylwianowak/memsandgifs/MainActivity.java
@@ -19,10 +19,12 @@ public class MainActivity extends FragmentActivity {
 
     private static final int SPLASH = 0;
     private static final int SELECTION = 1;
-    private static final int FRAGMENT_COUNT = SELECTION +1;
+    private static final int SETTINGS = 2;
+    private static final int FRAGMENT_COUNT = SETTINGS +1;
     private boolean isResumed = false;
     private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
     private UiLifecycleHelper uiHelper;
+    private MenuItem settings;
     private Session.StatusCallback callback =
             new Session.StatusCallback() {
                 @Override
@@ -42,6 +44,7 @@ public class MainActivity extends FragmentActivity {
         FragmentManager fm = getSupportFragmentManager();
         fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
         fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+        fragments[SETTINGS] = fm.findFragmentById(R.id.userSettingsFragment);
 
         FragmentTransaction transaction = fm.beginTransaction();
         for(int i = 0; i < fragments.length; i++) {
@@ -98,27 +101,14 @@ public class MainActivity extends FragmentActivity {
         uiHelper.onSaveInstanceState(outState);
     }
 
-  /*  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (item.equals(settings)) {
+            showFragment(SETTINGS, true);
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
-    }*/
+        return false;
+}
 
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
         // Only make changes if the activity is visible
@@ -142,6 +132,21 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // only add the menu when the selection fragment is showing
+        if (fragments[SELECTION].isVisible()) {
+            if (menu.size() == 0) {
+                settings = menu.add(R.string.settings);
+            }
+            return true;
+        } else {
+            menu.clear();
+            settings = null;
+        }
+        return false;
+    }
+
     private void showFragment(int fragmentIndex, boolean addToBackStack) {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
@@ -157,4 +162,5 @@ public class MainActivity extends FragmentActivity {
         }
         transaction.commit();
     }
+
 }
